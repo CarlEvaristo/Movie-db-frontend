c86323d29fc8c03c5119a48fb06dd36b650a6f1e
CarlEverest78 <thepythoneer@gmail.com>
2022-07-11 10:36:52 +0200

    final commit

--- index.js
+++ index.js
@@ -1,46 +1,43 @@
-/* 
-    TV Guide
-    
-    Write an async function called findShow(query)
-        performs a fetch call to:
-        https://api.tvmaze.com/singlesearch/shows?q=${query}&embed=seasons
-        and returns the resulting show object
-        
-    Build a layout to display the show
-        - Title
-        - Summary
-        - Seasons listed as individual divs
-*/
-let query = "a-team"
 
-async function findShow() {
+function buttonHandler() {
+    const inputField = document.getElementById("input-field")
+    const submitBtn = document.getElementById("submit-btn")
+    submitBtn.addEventListener("click", function(event) {
+        let inputData = inputField.value
+        getMovieHtml(inputData) 
+    })
+}
+
+async function findShow(query) {
     let response = await fetch(`https://api.tvmaze.com/singlesearch/shows?q=${query}&embed=seasons`)
     let data = await response.json()
     return data  //data._embedded.seasons
 }
 
-function getMovieHtml() {
-    findShow().then(movie => {
+function getMovieHtml(query) {
+    findShow(query).then(movie => {
         let seasons = movie._embedded.seasons
         let seasonsHtml = seasons.map(season => {
-            console.log(season.image)
             return `
             <div>
+                <a href="${season.url}" target="blank"><img class="imageBox" src="${season.image.medium}"></a>
                 <p>Season ${season.number}</p>
-                <img class="imageBox" src="${season.image.medium}">
             </div>
             `
         }).join("")
-        console.log(seasonsHtml)
-        document.getElementById("gridContainer").innerHTML += `
-            <div class="movie">
-                <h1>${movie.name}</h1>
-                <p>${movie.summary}</p>
-                <div id="seasonsBox">${seasonsHtml}</div>
-
+ 
+        document.getElementById("gridContainer").innerHTML = `
+            <h1>${movie.name}</h1>
+            <div>
+                <input id="input-field" type="text" name="inputMovie" placeholder="Search TV Series">
+                <button id="submit-btn" type="submit">Search</button>
             </div>
+            <span class="summary">${movie.summary}</span>
+            <div id="seasonsBox">${seasonsHtml}</div>
         `
+        buttonHandler()
     })
+
 }
 
-getMovieHtml()
+buttonHandler()
